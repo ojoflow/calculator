adb53e0fc05d4526118f7e4939b93ff3f27b6952
Oscar <90513703+ojoflow@users.noreply.github.com>
2022-02-07 17:26:28 +0000

    Add files via upload
    The beginning of adding functionality to the calculator.

---
+++ calculator.js
@@ -0,0 +1,21 @@
+let numbers = Array.from(document.querySelectorAll('.number'));
+console.log(numbers);
+console.log(numbers[0]);
+const add = (x,y) => {
+    return x+y
+}
+
+const subtract = (x,y) => {
+    return x-y;
+}
+
+const multiply = (x,y) => {
+    return x*y;
+}
+
+const divide = (x,y) => {
+    return x/y;
+}
+const operator = (x,y,operand) => {
+
+}
