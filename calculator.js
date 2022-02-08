f84129dc156601e9ccdc97ccc06764917904d0c4
Oscar <90513703+ojoflow@users.noreply.github.com>
2022-02-08 19:22:23 +0000

    Add files via upload
    Added keyboard functionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+//initalize an equation object
 const equation = {
     operator: null,
     firstNum: null,
@@ -27,6 +28,7 @@ function operate(operator, num1, num2) {
     }
 }
 
+//functionality for the C button
 function clear() {
     displayExpression = "0";
     displaySolution =  "";
@@ -37,6 +39,7 @@ function clear() {
     solution.textContent = displaySolution;
 }
 
+//functionality for the ⌫ button
 function backSpace() {
     displayExpression = displayExpression.toString().slice(0, -1);
     if (displayExpression === "") {
@@ -55,70 +58,86 @@ function display(contentSol, contentExp) {
 }
 
 const buttons = document.querySelectorAll("button");
+function calculate(content) {
+    let isNumber = !isNaN(Number(content));
+    let isDot = (content === ".");
+    let isEquationFull = (equation.firstNum !== null
+                    && equation.operator !== null
+                    && equation.secondNum !== null);
 
+    if (content === "C") {
+        clear();
+        return;
+    }
+    if (isNumber) {
+        if(equation.firstNum !== null && equation.operator !== null && equation.secondNum === null) {
+            displayExpression = "0";
+            equation.secondNum = 0;
+    }
+        if (displayExpression !== "0") {
+            displayExpression += content;
+        } else {
+            displayExpression = content;
+        }
+    } else if (isDot) {
+        if (displayExpression.toString().includes(content) === false) {
+        displayExpression += content;
+        }
+    } else {
+        if (content === "⌫") {
+            backSpace();
+            return
+        }
+        if (isEquationFull) {
+            equation.secondNum = toNumFit(Number(displayExpression));
+            if (equation.secondNum === 0 && equation.operator === "/") {
+                solution.textContent = "Can't divide a number with 0!";
+                return;
+            }
+            if (content === "=") {
+                displayExpression = toNumFit(operate(equation.operator, equation.firstNum, equation.secondNum));
+                displaySolution = `${equation.firstNum} ${equation.operator} ${equation.secondNum} =`;
+                equation.firstNum = toNumFit(operate(equation.operator, equation.firstNum, equation.secondNum));
+                equation.operator = null;
+                equation.secondNum = null;
+                display(displayExpression, displaySolution);
+                return;
+            } else {
+                equation.firstNum = toNumFit(operate(equation.operator, equation.firstNum, equation.secondNum));
+                equation.operator = content;
+                equation.secondNum = null;
+                displayExpression = equation.firstNum;
+                displaySolution = `${equation.firstNum} ${equation.operator}`;
+                display(displayExpression, displaySolution);
+                return;
+        }
+    }
+        if (equation.firstNum === null) {
+            equation.firstNum = toNumFit(Number(displayExpression));
+        }
+        equation.operator = content;
+        equation.firstNum = toNumFit(Number(displayExpression));
+        displaySolution = `${equation.firstNum} ${equation.operator}`;
+    }
+    display(displayExpression, displaySolution);
+}
+//button click
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         let content = e.target.textContent;
-        let isNumber = !isNaN(Number(content));
-        let isDot = (content === ".");
-        let isEquationFull = (equation.firstNum !== null
-                            && equation.operator !== null
-                            && equation.secondNum !== null);
-
-        if (content === "C") {
-            clear();
-            return;
-        }
-        if (isNumber) {
-            if(equation.firstNum !== null && equation.operator !== null && equation.secondNum === null) {
-                displayExpression = "0";
-                equation.secondNum = 0;
-            }
-            if (displayExpression !== "0") {
-                displayExpression += content;
-            } else {
-                displayExpression = content;
-            }
-        } else if (isDot) {
-            if (displayExpression.toString().includes(content) === false) {
-                displayExpression += content;
-            }
-        } else {
-            if (content === "⌫") {
-                backSpace();
-                return
-            }
-            if (isEquationFull) {
-                equation.secondNum = toNumFit(Number(displayExpression));
-                if (equation.secondNum === 0 && equation.operator === "/") {
-                    solution.textContent = "Can't divide a number with 0!";
-                    return;
-                }
-                if (content === "=") {
-                    displayExpression = toNumFit(operate(equation.operator, equation.firstNum, equation.secondNum));
-                    displaySolution = `${equation.firstNum} ${equation.operator} ${equation.secondNum} =`;
-                    equation.firstNum = toNumFit(operate(equation.operator, equation.firstNum, equation.secondNum));
-                    equation.operator = null;
-                    equation.secondNum = null;
-                    display(displayExpression, displaySolution);
-                    return;
-                } else {
-                    equation.firstNum = toNumFit(operate(equation.operator, equation.firstNum, equation.secondNum));
-                    equation.operator = content;
-                    equation.secondNum = null;
-                    displayExpression = equation.firstNum;
-                    displaySolution = `${equation.firstNum} ${equation.operator}`;
-                    display(displayExpression, displaySolution);
-                    return;
-                }
-            }
-            if (equation.firstNum === null) {
-                equation.firstNum = toNumFit(Number(displayExpression));
-            }
-            equation.operator = content;
-            equation.firstNum = toNumFit(Number(displayExpression));
-            displaySolution = `${equation.firstNum} ${equation.operator}`;
-        }
-        display(displayExpression, displaySolution);
+        calculate(content);
     });
 })
+//key press
+function keyPress(e) {
+    let key = document.querySelector(`button[data-key="${e.key}"]`);
+    if(!key) return;
+    key.classList.add('pressed')
+    setTimeout(function () {
+        key.classList.remove('pressed')
+    }, 100)
+    
+    let content = key.textContent;
+    calculate(content);
+}
+window.addEventListener('keydown', keyPress);
